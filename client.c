811b48532c8639cef09d69fe7013eaea6563ada0
nyagalen <90185116+nyagalen@users.noreply.github.com>
2023-08-31 15:42:01 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -3,28 +3,74 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svydrina <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: svetlana <svetlana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/17 18:59:46 by svydrina          #+#    #+#             */
-/*   Updated: 2023/07/21 18:28:28 by svydrina         ###   ########.fr       */
+/*   Created: 2023/08/25 18:04:05 by svydrina          #+#    #+#             */
+/*   Updated: 2023/08/31 15:23:56 by svydrina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <signal.h>
-#include "libft/libft.h"
+#include "minitalk.h"
+
+int	g_received = -1;
 
 static void	action(int sig)
 {
-	static int	received = 0;
+	static int	int_count = 32;
+	static int	char_count = 8;
+	static char	c = 0;
 
 	if (sig == SIGUSR1)
-		received++;
+		g_received = 1;
+	else if (sig == SIGUSR2)
+		g_received = 0;
+	if (int_count > 0)
+		int_count--;
 	else
 	{
-		ft_putnbr_fd(received, 1);
-		ft_putchar_fd('\n', 1);
-		exit(0);
+		if (char_count--)
+			c += g_received;
+		if (char_count == 0)
+		{
+			ft_putchar_fd(c, 1);
+			char_count = 8;
+			c = 0;
+		}
+		else
+			c <<= 1;
+	}
+}
+
+static int	send_bit(int pid, int c_or_i, int i)
+{
+	int	sig;
+
+	if (c_or_i >> i & 1)
+		sig = SIGUSR1;
+	else
+		sig = SIGUSR2;
+	g_received = -1;
+	if (kill(pid, sig))
+		return (-1);
+	sig = -1;
+	while (g_received == -1 && ++sig <= 200)
+		usleep(1);
+	return (g_received);
+}
+
+static void	send_len(int pid, int len)
+{
+	int	i;
+
+	i = 32;
+	while (i--)
+	{
+		if (send_bit(pid, len, i) < 0)
+		{
+			ft_printf("Error");
+			exit(0);
+		}
+		usleep(50);
 	}
 }
 
@@ -33,40 +79,42 @@ static void	send_bits(int pid, char *str)
 	int		i;
 	char	c;
 
+	send_len(pid, ft_strlen(str));
 	while (*str)
 	{
 		c = *str++;
 		i = 8;
 		while (i--)
 		{
-			if (c >> i & 1)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			usleep(100);
+			if (send_bit(pid, c, i) < 0)
+			{
+				ft_printf("Error");
+				exit(0);
+			}
+			usleep(50);
 		}
 	}
-	i = 8;
-	while (i--)
-	{
-		kill(pid, SIGUSR2);
-		usleep(100);
-	}
+	exit(0);
 }
 
 int	main(int argc, char **argv)
 {
 	int	pid;
 
+	if (argc != 3)
+	{
+		ft_printf("Wrong number of arguments, try again");
+		return (1);
+	}
 	pid = ft_atoi(argv[1]);
 	if (!pid)
+	{
+		ft_printf("Wrong PID");
 		return (1);
-	if (argc != 3 || !ft_strlen(argv[2]))
-		return (1);
-	ft_putstr_fd("Sent: ", 1);
-	ft_putnbr_fd(ft_strlen(argv[2]), 1);
-	ft_putchar_fd('\n', 1);
-	ft_putstr_fd("Received: ", 1);
+	}
+	ft_printf("client's pid: %u\n", getpid());
+	ft_printf("Sending %d symbols\n", ft_strlen(argv[2]));
+	ft_printf("Server received message: ");
 	signal(SIGUSR1, action);
 	signal(SIGUSR2, action);
 	send_bits(pid, argv[2]);
